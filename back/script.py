09c42b51ee77ba28129e4efc0fcb6463c8858015
kit <mr.marashli@gmail.com>
2020-03-26 14:13:55 +0300

    add levenshtein distance function

--- back/script.py
+++ back/script.py
@@ -2,22 +2,41 @@ from flask import Flask, request
 import config
 
 
-app = Flask(__name__)
+def distance(a, b):
+    n, m = len(a), len(b)
+    if n > m:
+        a, b = b, a
+        n, m = m, n
 
-@app.after_request
-def after_request(response):
-    r"""Fix access"""
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+    current_row = range(n + 1)
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            if a[j - 1] != b[i - 1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
 
-@app.route('/getMessage', methods=['GET'])
-def data_update():
-    message = request.args.get("message")
-    bot_message = message[::-1]
+    return current_row[n]
 
-    return bot_message
+if __name__ == '__main__':
+	app = Flask(__name__)
+
+	@app.after_request
+	def after_request(response):
+	    r"""Fix access"""
+	    response.headers.add('Access-Control-Allow-Origin', '*')
+	    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+	    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+	    return response
+
+	@app.route('/getMessage', methods=['GET'])
+	def get_message():
+	    message = request.args.get("message")
+
+	    bot_message = message[::-1]
+
+	    return bot_message
 
 
-app.run(debug=True, host=config.HOST_IP, port=config.PORT)
+	app.run(debug=True, host=config.HOST_IP, port=config.PORT)
