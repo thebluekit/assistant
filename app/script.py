24d7bda628e69af211ad799fd0cd4c7501e35c76
kit <mr.marashli@gmail.com>
2020-03-29 03:47:33 +0300

    add OOP base

--- app/script.py
+++ app/script.py
@@ -1,47 +1,15 @@
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
-from fuzzywuzzy import fuzz
-from copy import deepcopy
-import datetime
 import os
 
-opts = {
-    "cmds": {
-    }
-}
-
-
-def recognize_cmd(message):
-    message_li = converted_message(message)
-    rc = {'cmd': '', "recognized_words": 0}
-    for command, key_words in opts['cmds'].items():
-        rc_tmp = {"cmd": command, "recognized_words": 0}
-        for key_word in key_words:
-            for word in message_li:
-                vrt = fuzz.ratio(word, key_word)
-                if vrt > 90:
-                    rc_tmp["recognized_words"] += 1
-        if rc_tmp['recognized_words'] > rc["recognized_words"]:
-            rc = deepcopy(rc_tmp)
-
-    if rc["recognized_words"] / len(message_li) < 0.6666:
-        rc = {'cmd': '', "recognized_words": 0}
-    return rc
-
-
-def converted_message(message):
-    message_li = message.split(' ')
-    return message_li
-
-
-def execute_cmd(cmd):
-    return "command not found"
-
+from assistant import Assistant
 
 if __name__ == '__main__':
     load_dotenv()
     HOST_IP = os.getenv("HOST_IP")
     PORT = os.getenv("PORT")
+
+    bot = Assistant()
 
     app = Flask(__name__)
 
@@ -64,8 +32,7 @@ if __name__ == '__main__':
     @app.route('/getMessage', methods=['GET'])
     def get_message():
         message = request.args.get("message")
-        cmd = recognize_cmd(message)
-        bot_message = execute_cmd(cmd['cmd'])
+        bot_message = bot.response(message)
         return bot_message
 
     app.run(debug=True, host=HOST_IP, port=PORT)
